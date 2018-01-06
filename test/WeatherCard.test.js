88c2aeee976c45d115a00c3f5091d9f523974e44
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-06 14:22:38 -0700

    Basic weathercard testing complete

--- test/WeatherCard.test.js
+++ test/WeatherCard.test.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import App from '../lib/App.js';
+import WeatherCard from '../lib/WeatherCard.js';
 import mockData from '../__mocks__/mockData2.js';
 
-describe('App', () => {
+let tenDayFcst = mockData.forecast.simpleforecast.forecastday[0];
+let sevenHrFcst = [];
 
+for (let i = 0; i < 7; i++) {
+  sevenHrFcst.push(mockData.hourly_forecast[i]);
+};
+
+describe('WeatherCard', () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  })
-
   it('should exist', () => {
+    wrapper = shallow(<WeatherCard key={0} cardType='hour-card' fcst={sevenHrFcst[0]}/>);
+
     expect(wrapper).toBeDefined();
   })
 
-  
+  it('should take props', () => {
+    //key is not a prop -- why?? Is this a react thing?
+    // expect(wrapper.instance().props.key).toEqual(0);
+    expect(wrapper.instance().props.cardType).toEqual('hour-card');
+    expect(wrapper.instance().props.fcst).toEqual(sevenHrFcst[0]);
+  })
 
+  describe('sevenHourCard', () => {
+
+    beforeEach(() => {
+      wrapper = shallow(<WeatherCard key={0} cardType='hour-card' fcst={sevenHrFcst[0]}/>);
+    })
+
+    it('should render a seven hour card', () => {
+      expect(wrapper.find('h3').length).toEqual(2);
+      expect(wrapper.find('h3').first().text()).toEqual('Hour of the Beetle');
+
+      expect(wrapper.find('img').length).toEqual(1);
+    })
+    
+  })
+
+  describe('tenDayCard', () => {
+
+    beforeEach(() => {
+      wrapper = shallow(<WeatherCard key={0} cardType='TenDay' fcst={tenDayFcst}/>);
+    })
+
+    it('should take props', () => {
+      expect(wrapper.instance().props.cardType).toEqual('TenDay');
+      expect(wrapper.instance().props.fcst).toEqual(tenDayFcst);
+    })
+
+    it('should render a ten day card', () => {
+      expect(wrapper.find('h3').length).toEqual(3);
+      expect(wrapper.find('h3').first().text()).toEqual('Thursday');
+
+      expect(wrapper.find('img').length).toEqual(1);
+    })
+  })
 })
