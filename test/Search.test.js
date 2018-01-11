d1ffa4c3dfa6bf49d58f56b9cc3a63fdeafd13de
Natalie Montoya <nataliesbarron@gmail.com>
2018-01-10 20:45:46 -0700

    Effort to test state change

--- test/Search.test.js
+++ test/Search.test.js
@@ -37,12 +37,22 @@ describe('Search', () => {
 
   it('should render suggestions that match text of suggestions array', () => {
     wrapper = mount(<Search getWeather={mockFunction} />)
-    
+
     wrapper.setState({ suggestions: ['Denver, CO', 'Denton, TX']});
     expect(wrapper.find('Suggestion').first().text()).toEqual('Denver, CO');
 
     wrapper.setState({ suggestions: ['Denton, TX', 'Denver, CO']});
     expect(wrapper.find('Suggestion').first().text()).toEqual('Denton, TX');
 
+  })
+
+  it('should a list of suggestions upon input', () => {
+    wrapper = mount(<Search getWeather={mockFunction} />)
+    const event = {target: {value: "Denv"}};
+
+    wrapper.instance().onType(event);
+    expect(wrapper.state('Suggestion')).to.equal('Denver, CO')
+    // wrapper.find('input').simulate('change', event);
+    // expect(wrapper.find('Suggestions').first()).toEqual('Denver, CO')
   })
 })
